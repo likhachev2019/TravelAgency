3a26374498fe2a64d96c4c2150e7f0324997590f
notn0name <lixachev.fedor@gmail.com>
2019-08-07 11:30:44 +0400

    Initial commit

---
+++ src/main/java/TravelAgency/TravelAgencyApplication.java
@@ -0,0 +1,13 @@
+package TravelAgency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAgencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelAgencyApplication.class, args);
+	}
+
+}
---
+++ src/main/java/TravelAgency/controller/Controller.java
@@ -0,0 +1,13 @@
+package TravelAgency.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class Controller{
+
+
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	public String main(){
+		return "Helo .com";
+	}
+}
